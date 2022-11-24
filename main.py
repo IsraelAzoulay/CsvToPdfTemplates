28893a5b68d104d0a3e503f26a9c27fc23c09209
IsraelAzoulay <israelazoulay1@gmail.com>
2022-11-24 15:30:17 +0200

    Add more pages for each topic.

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ pdf = FPDF(orientation="portrait", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    # Creating the pdf page with the "add_page()" func which adds page to the
-    # document and belongs to the FPDF class.
+    # Creating\adding a pdf page (with the "add_page()" func which adds page to the
+    # document and belongs to the FPDF class).
     pdf.add_page()
     # Setting the font styles properties of a generic cell's content.
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     # Creating a single cell.
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=1)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
+
+    for page in range(row["Pages"] - 1):
+        pdf.add_page()
 
 # Generating the pdf file on the disk.
 pdf.output("output.pdf")
